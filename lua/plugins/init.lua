90190aa4c3b86a4142c01136fe082fad02d5843d
Mike Boiko <mike@boiko.ca>
2025-01-04 12:23:43 -0700

    feat: add vim-startuptime plugin
    Add vim-startuptime plugin to measure and analyze Neovim startup times.
    Configure plugin to run benchmarks with 10 iterations for more accurate
    measurements.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,12 @@
 return {
   { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
+  {
+    'dstein64/vim-startuptime',
+    -- lazy-load on a command
+    cmd = 'StartupTime',
+    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
+    init = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
 }
