632a42ba8d3bd6690350e42a17c048709277e096
Mike Boiko <mike@boiko.ca>
2025-01-04 21:58:32 -0700

    feat: add dap debugging with one-small-step-for-vimkind
    Add support for debugging lua programs with one-small-step-for-vimkind.
    Configure treesitter to support debugging capabilities and initializes
    debugger on port 8086 when init_debug is set.

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 require('config.lazy')
 
+-- one-small-step-for-vimkind nvim-dap debugging
+if init_debug then
+  require('osv').launch({ port = 8086, blocking = true })
+end
+
 -- TODO-MB [250104] - convert all of these to lua
 vim.cmd('source ~/.config/nvim/vimscript/functions.vim')
 
---
+++ lua/plugins/dap.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'mfussenegger/nvim-dap-python', 'nvim-neotest/nvim-nio' },
+    config = function()
+      require('dapui').setup()
+    end,
+  },
+  {
+    'jbyuki/one-small-step-for-vimkind',
+    config = function()
+      local dap = require('dap')
+
+      dap.configurations.lua = {
+        {
+          type = 'nlua',
+          request = 'attach',
+          name = 'Attach to running Neovim instance',
+        },
+      }
+
+      dap.adapters.nlua = function(callback, config)
+        callback({ type = 'server', host = config.host or '127.0.0.1', port = config.port or 8086 })
+      end
+
+      vim.keymap.set('n', '<leader>db', require('dap').toggle_breakpoint, { noremap = true })
+      vim.keymap.set('n', '<leader>dc', require('dap').continue, { noremap = true })
+      vim.keymap.set('n', '<leader>do', require('dap').step_over, { noremap = true })
+      vim.keymap.set('n', '<leader>di', require('dap').step_into, { noremap = true })
+
+      vim.keymap.set('n', '<leader>dl', function()
+        require('osv').launch({ port = 8086 })
+      end, { noremap = true })
+    end,
+  },
+}
