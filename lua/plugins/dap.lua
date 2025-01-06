919f7dac9c0305a6262989993c0aac35a247ce42
Mike Boiko <mike@boiko.ca>
2025-01-06 13:07:04 -0700

    feat(debug): migrate from nvim-dap to vimspector
    Temporarily comment out nvim-dap keymaps and add vimspector with support for
    Python, Go, C#, and Node.js debugging. Installation script automatically
    configures required debug adapters.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -23,19 +23,29 @@ return {
         callback({ type = 'server', host = config.host or '127.0.0.1', port = config.port or 8086 })
       end
 
-      vim.keymap.set(
-        'n',
-        '<leader>db',
-        require('dap').toggle_breakpoint,
-        { noremap = true, desc = 'dap: toggle_breakpoint' }
-      )
-      vim.keymap.set('n', '<leader>dc', require('dap').continue, { noremap = true, desc = 'dap: continue' })
-      vim.keymap.set('n', '<leader>do', require('dap').step_over, { noremap = true, desc = 'dap: step_over' })
-      vim.keymap.set('n', '<leader>di', require('dap').step_into, { noremap = true, desc = 'dap: step_into' })
-
-      vim.keymap.set('n', '<leader>dl', function()
-        require('osv').launch({ port = 8086 })
-      end, { noremap = true, desc = 'dap: launch neovim lua server' })
+      -- TODO-MB [250106] - Restore after deprecating vimspector
+      -- vim.keymap.set(
+      --   'n',
+      --   '<leader>db',
+      --   require('dap').toggle_breakpoint,
+      --   { noremap = true, desc = 'dap: toggle_breakpoint' }
+      -- )
+      -- vim.keymap.set('n', '<leader>dc', require('dap').continue, { noremap = true, desc = 'dap: continue' })
+      -- vim.keymap.set('n', '<leader>do', require('dap').step_over, { noremap = true, desc = 'dap: step_over' })
+      -- vim.keymap.set('n', '<leader>di', require('dap').step_into, { noremap = true, desc = 'dap: step_into' })
+      -- vim.keymap.set('n', '<leader>dl', function()
+      --   require('osv').launch({ port = 8086 })
+      -- end, { noremap = true, desc = 'dap: launch neovim lua server' })
+    end,
+  }, -- }}}
+  { -- {{{1
+    'puremourning/vimspector',
+    build = function()
+      local install_cmd = './install_gadget.py'
+      vim.fn.system(install_cmd .. ' --enable-python')
+      vim.fn.system(install_cmd .. ' --enable-go --update-gadget-config')
+      vim.fn.system(install_cmd .. ' --force-enable-csharp --update-gadget-config')
+      vim.fn.system(install_cmd .. ' --force-enable-node --update-gadget-config')
     end,
   }, -- }}}
 }
