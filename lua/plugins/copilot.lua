070c222aaf0a59b3fed3e794244f3e4d91bfcf7f
Mike Boiko <mike@boiko.ca>
2025-01-07 18:34:00 -0700

    build: enable tiktoken build for MacOS and Linux
    Uncomments the tiktoken build step which is required for CopilotChat.nvim to
    work correctly on MacOS and Linux systems.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -27,7 +27,7 @@ return {
       { 'zbirenbaum/copilot.lua' },
       { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
     },
-    -- build = 'make tiktoken', -- Only on MacOS or Linux
+    build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
       -- See Configuration section for options
     },
