d6d7ec93cd49fb2704e3a9b1edcf42bbe44fd3c8
Mike Boiko <mike@boiko.ca>
2025-01-08 17:45:03 -0700

    feat: add which-key plugin
    Adds an elegant popup for keymaps to enhance discoverability. The plugin is
    configured to show buffer local keymaps with <leader>? key.

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -12,5 +12,23 @@ return {
   { -- vim-ps1: powershell syntax {{{1
     'PProvost/vim-ps1',
   }, -- }}}
+  { -- which-key.nvim {{{1
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show({ global = false })
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
+      },
+    },
+  }, -- }}}
 }
 -- vim: foldmethod=marker:foldlevel=1
