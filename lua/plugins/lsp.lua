4a75b93e679a716c08826c45c00eb053434a3e07
Mike Boiko <mike@boiko.ca>
2025-01-12 07:43:17 -0700

    docs: add descriptive help text for LSP keymaps
    Add descriptive help text for LSP keymaps to improve discoverability of LSP
    features through which-key and built-in help system. Each keymap now includes
    a clear explanation of its functionality.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -130,21 +130,50 @@ return {
       })
 
       -- Key mappings
-      local opts = {}
       vim.keymap.set(
         'n',
         '<Leader>se',
         '<cmd>lua vim.diagnostic.setqflist()<CR>',
         { silent = true, desc = 'Set diagnostics in quickfix list' }
       )
-      vim.keymap.set('n', '<leader>fr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-      vim.keymap.set('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-      vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-      vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
-      vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-      vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-      vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-      vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+      vim.keymap.set(
+        'n',
+        '<leader>fr',
+        '<cmd>lua vim.lsp.buf.references()<cr>',
+        { desc = 'Find all references of symbol under cursor' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>h',
+        '<cmd>lua vim.lsp.buf.hover()<cr>',
+        { desc = 'Show hover documentation for symbol under cursor' }
+      )
+      vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', { desc = 'Jump to previous diagnostic' })
+      vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', { desc = 'Jump to next diagnostic' })
+      vim.keymap.set(
+        'n',
+        'gD',
+        '<cmd>lua vim.lsp.buf.declaration()<cr>',
+        { desc = 'Jump to declaration of symbol under cursor' }
+      )
+      vim.keymap.set(
+        'n',
+        'gR',
+        '<cmd>lua vim.lsp.buf.rename()<cr>',
+        { desc = 'Rename symbol under cursor across workspace' }
+      )
+      vim.keymap.set(
+        'n',
+        'gd',
+        '<cmd>lua vim.lsp.buf.definition()<cr>',
+        { desc = 'Jump to definition of symbol under cursor' }
+      )
+      vim.keymap.set(
+        'n',
+        'gi',
+        '<cmd>lua vim.lsp.buf.implementation()<cr>',
+        { desc = 'Find implementations of interface/class under cursor' }
+      )
 
       -- Format on save
       vim.api.nvim_create_autocmd('BufWritePre', {
