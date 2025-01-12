922ba02b34fd895df87962e7f0789204ac2477f2
Mike Boiko <mike@boiko.ca>
2025-01-12 07:36:19 -0700

    fix: remove redundant task about pyright not working
    As task is no longer needed since it was resolved in a previous commit

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -137,11 +137,14 @@ return {
         '<cmd>lua vim.diagnostic.setqflist()<CR>',
         { silent = true, desc = 'Set diagnostics in quickfix list' }
       )
-      vim.keymap.set('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
       vim.keymap.set('n', '<leader>fr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-      vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+      vim.keymap.set('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
       vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
       vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+      vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+      vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+      vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+      vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
 
       -- Format on save
       vim.api.nvim_create_autocmd('BufWritePre', {
