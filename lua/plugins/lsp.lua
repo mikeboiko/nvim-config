a8c61685b727cdfe4349c7ac20996559adc5f7a1
Mike Boiko <mike@boiko.ca>
2025-12-27 06:29:33 -0700

    feat(folding): add nvim-origami plugin
    Add chrisgrieser/nvim-origami to manage code folding. Load the
    plugin lazily and disable Vim's automatic folding by setting
    foldlevel and foldlevelstart.
    
    Add .luarc.json to suppress some Lua diagnostics, and update
    lazy-lock.json to pin the new plugin commit.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -362,5 +362,15 @@ return {
       bundle_path = '/opt/powershell-editor-services',
     },
   }, -- }}}
+  { -- nvim-origami folding {{{1
+    'chrisgrieser/nvim-origami',
+    event = 'VeryLazy',
+    opts = {}, -- needed even when using default config
+
+    -- recommended: disable vim's auto-folding
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  }, -- }}}
 }
--- vim: foldmethod=marker:foldlevel=1
